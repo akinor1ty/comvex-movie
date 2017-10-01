87913100838dcfb8c520f28a1174967a43f76ec7
fujiwaraakinori <foxyfox64@gmail.com>
2017-10-01 23:08:43 +0900

    Adds testing libraries and some sample testings.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import assert from 'power-assert';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// For react v16, enzyme need this Adaptor so far.
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('has App class.', () => {
+  const wrapper = shallow(<App/>);
+  assert(wrapper.find('.App').length === 1);
 });
