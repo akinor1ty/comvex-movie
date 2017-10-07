4f02994f16799425064c739b7c3c946fa8d7ec89
fujiwaraakinori <foxyfox64@gmail.com>
2017-10-07 17:24:59 +0900

    Changes redux-promise to redux-thunk for middleware.

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from '../reducers/index';
-import promiseMiddleware from 'redux-promise';
+import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
 
-export default createStore(reducers, applyMiddleware(promiseMiddleware, logger));
+export default createStore(reducers, applyMiddleware(thunkMiddleware, logger));
