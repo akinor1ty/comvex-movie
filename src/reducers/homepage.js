135b9d6ca2ee25bd48ffae502ca77d9ce42a3cc6
fujiwaraakinori <foxyfox64@gmail.com>
2017-10-08 17:15:30 +0900

    Adds homepage reducer.

--- src/reducers/homepage.js
+++ src/reducers/homepage.js
@@ -1,31 +1,117 @@
 // @flow
 import * as ActionsTypes from '../constants/ActionTypes';
 import type { CounterAction } from '../actions/homepage';
+import type { Movie } from '../types/home';
 
-type Movie = {
-  backdrop_path: string,
+type ResultMovie = {
+  id: number,
+  original_title: string,
+  poster_path: string,
+  genre_ids: Array<number>,
+  vote_average: number,
+  popularity: number,
 }
+
+
 type HomeState = {
+  page: number,
   movies: Array<Movie>,
+  genres: Array<{ id: number, name: string}>
 }
 
 const initialState: HomeState = {
-  movies: []
+  page: 0,
+  movies: [],
+  genres: [],
+  sortOptions: [
+    { value: 'original_title.asc', label: 'Title' },
+    { value: 'release_date.desc', label: 'Release date' },
+    { value: 'vote_average.desc', label: 'Vote average' },
+    { value: 'popularity.desc', label: 'Popularity' },
+  ],
+  sortBy: null,
+  filterWith: null,
+  searchQuery: null,
+  searching: false,
 };
+
+function mapMovies(results: Array<ResultMovie>, genres: Array<any>) {
+
+  return results.map(r => ({
+      id: r.id,
+      title: r.original_title,
+      posterPath: r.poster_path,
+      genres: r.genre_ids.filter(id => genres.map(g => g.id).includes(id)).map(id => genres.find(g => g.id === id)),
+      voteAverage: r.vote_average,
+      popularity: r.popularity,
+    })
+  );
+}
 
 export default (state: HomeState = initialState, action: CounterAction) => {
 
   switch(action.type) {
 
-    case ActionsTypes.GET_MOVIES:
-      const { payload } = action;
+    case ActionsTypes.GET_GENRES_SUCCESS: {
+      const { data } = action;
+      return {
+        ...state,
+        genres: data.genres
+      };
+    }
+
+    case ActionsTypes.GET_MOVIES_SUCCESS: {
+      const { page, results } = action.payload;
+      const movies = mapMovies(results, state.genres);
 
       return {
         ...state,
-        movies: payload.results
+        page,
+        movies: page === 1 ? movies : [ ...state.movies, ...movies ],
       };
+    }
 
-    default:
+    case ActionsTypes.SEARCH_MOVIES_SUCCESS: {
+      const { page, results } = action.payload;
+      const movies = mapMovies(results, state.genres);
+
+      return {
+        ...state,
+        page,
+        movies: page === 1 ? movies : [ ...state.movies, ...movies ],
+      };
+    }
+
+    case ActionsTypes.SET_SORTING: {
+      const { value } = action;
+      return {
+        ...state,
+        page: 0,
+        sortBy: value,
+        searching: false,
+      };
+    }
+    case ActionsTypes.SET_FILTERING: {
+      const { value } = action;
+      return {
+        ...state,
+        page: 0,
+        filterWith: value,
+        searching: false,
+      };
+    }
+    case ActionsTypes.SET_SEARCH_QUERY: {
+      const { searchQuery } = action;
+      return {
+        ...state,
+        page: 0,
+        searchQuery: searchQuery,
+        searching: true,
+      }
+    }
+
+    default: {
       return state;
+    }
   }
 }
