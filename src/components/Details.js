7ca4b558baeb83a2b38cb058d2cc0eaacf1c33a5
fujiwaraakinori <foxyfox64@gmail.com>
2017-10-15 12:23:54 +0900

    Adds details page styling.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Grid, Row, Col }from 'react-flexbox-grid';
 const baseUrl = 'https://image.tmdb.org/t/p/w500';
 
 class Details extends Component {
@@ -17,48 +18,103 @@ class Details extends Component {
       voteAverage, backDrops
     } = this.props;
     return (
-      <div>
-        <Link to="/">
-          <button id="back-to-home">Back to list</button>
-        </Link>
-        <a className="arrow sample5-6">共通</a>
-        <div id="title">
-          { title }
-          <span id="release-year">{ `(${new Date(releaseDate).getFullYear()})` }</span>
-        </div>
-        <svg width="100" height="100" viewBox="0 0 42 42" class="donut">
-          <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
-          <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" stroke-width="3"></circle>
-          <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#ce4b99" stroke-width="3" stroke-dasharray={ `${voteAverage * 10} ${100 - voteAverage * 10}`} stroke-dashoffset="25"></circle>
-          <text x="21" y="21" text-anchor="middle" fontSize={8} dominant-baseline="central">{ voteAverage }</text>
-        </svg>
-        { overview }
-        <div id="vote-average">{ voteAverage }</div>
-        <div>
-          <label>Release date: </label>
-          <div>{releaseDate}</div>
-        </div>
-        <div>
-          <label>Genre: </label>
-            {
-              genres
-                .filter((g, i) => i < 2)
-                .map(g => <div>{ g.name }</div>)
-            }
-        </div>
-        <div>
-          <label>Duration: </label>
-          <div>{ `${runtime} min` }</div>
-        </div>
-        <ul id="cast">
-          { casts.filter((c, i) => i < 3).map(c => <li>{ `${c.name} As... ${c.character}` }</li>) }
-        </ul>
-        <div id="images">
-          { backDrops.map(bd => <img alt="image not found" src={ baseUrl + bd.filePath } />) }
-        </div>
-      </div>
+      <Grid>
+        <Row>
+          <Col xs={ 4 }>
+            <Row>
+              {
+                backDrops.map(bd => (
+                  <Col>
+                    <img width={ 200 } alt="image not found" src={ baseUrl + bd.filePath } />
+                  </Col>
+                ))
+              }
+            </Row>
+          </Col>
+          <Col xs={ 8 }>
+            <Link to="/">
+              <div className="back-to-home">
+                <a className="arrow arrow--left" />
+                <button className="button back-to-home__button">Back to list</button>
+              </div>
+            </Link>
+            <div className="sub-header details__sub-header">
+              <div className="heading details__sub-header__heading">
+                { title }
+                <span className="details__release-year">{ `(${new Date(releaseDate).getFullYear()})` }</span>
+              </div>
+            </div>
+            <div className="details__description">
+              { overview }
+            </div>
+            <div className="items">
+              <svg className="items__donut" viewBox="0 0 42 42">
+                <circle className="items__donut__ring"
+                        cx="21"
+                        cy="21"
+                        r="15.91549430918954" />
+                <circle className="items__donut__segment" cx="21" cy="21" r="15.91549430918954" stroke-dasharray={ `${voteAverage * 10} ${100 - voteAverage * 10}`} />
+                <text className="items__donut__number" x="21" y="21" >{ voteAverage }</text>
+              </svg>
+              <div className="items__smalls">
+                <div className="items__smalls__release-date">
+                  <label className="items__smalls__release-date__label">
+                    Release date
+                  </label>
+                  <div className="items__smalls__release-date__date">
+                    { releaseDate }
+                  </div>
+                </div>
+                <div className="items__smalls__genre">
+                  <label className="items__smalls__genre__label">
+                    Genre
+                  </label>
+                  <div className="items__smalls__genre__items">
+                    {
+                      genres
+                        .filter((g, i) => i < 2)
+                        .map(g => g.name)
+                        .join(',')
+                    }
+                  </div>
+                </div>
+                <div className="items__smalls__duration">
+                  <label className="items__smalls__duration__label">Duration</label>
+                  <div className="items__smalls__duration__duration">
+                    { `${runtime} min` }
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+              {
+                casts
+                  .filter((c, i) => i < 3)
+                  .map(c => (
+                    <ul className="cast">
+                      <li className="cast__actor" key={c.name}>
+                        <img className="cast__actor__image"
+                             src={ `https://image.tmdb.org/t/p/w75${c.profilePath}`}/>
+                        <span className="cast__actor___name">
+                        { `${c.name}` }
+                        </span>
+                        <span>
+                          As...
+                        </span>
+                        <span className="cast__actor___character">
+                          { `${c.character}` }
+                        </span>
+                      </li>
+                    </ul>
+                  ))
+              }
+
+          </Col>
+        </Row>
+      </Grid>
     );
   }
-};
+}
 
 export default Details;
