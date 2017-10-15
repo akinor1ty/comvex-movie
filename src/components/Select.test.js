97a4d5b570f9aa62b1933fbe6c57402bb666791e
fujiwaraakinori <foxyfox64@gmail.com>
2017-10-16 01:09:11 +0900

    Fixes obsolete tests.

--- src/components/Select.test.js
+++ src/components/Select.test.js
@@ -11,32 +11,28 @@ configure({ adapter: new Adapter() });
 
 describe('<Select />', () => {
   it('should render', () => {
-    const onChange = sinon.spy();
+    const onSelect = sinon.spy();
+
     const wrapper = shallow(<Select name="testname"
                                     label="labeltest"
-                                    onChange={ onChange }/>);
+                                    onSelect={ onSelect }
+                                    options={[]}
+    />);
     assert(wrapper.find('label').length === 1);
-    console.log(wrapper.find('label'));
-    assert(wrapper.find('select').length === 1);
+    assert(wrapper.find('.dropdown-wrapper').length === 1);
 
   });
 
   it('should kick on change event', () => {
-    const onChange = sinon.spy();
+    const onSelect = sinon.spy();
     const wrapper = shallow(<Select name="testname"
                                     label="labeltest"
-                                    onChange={ onChange }/>);
-    wrapper.find('select').simulate('change', { target: { value: 1 }});
-    assert(onChange.called, 'called change event');
-  });
-  
-  it('should children options', () => {
-    const onChange = sinon.spy();
-    const options = <option>a</option>;
-    const wrapper = shallow(<Select name="testname"
-                                    label="labeltest"
-                                    onChange={ onChange }>{options}</Select>);
-    assert(wrapper.find('option').length === 1);
+                                    onSelect={ onSelect }
+                                    options={[{ value: 1, label: 'test'}]}
+    />);
+    wrapper.find('.dropdown__option').simulate('click', 1);
+    assert(onSelect.called, 'called change event');
+    assert(wrapper.state().selected === 1);
   });
 
 
